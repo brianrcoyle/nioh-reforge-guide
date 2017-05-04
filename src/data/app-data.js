afc592134770eae9c5771d7cc51b051c4e51fcc8
COYLE, BRIAN R <brian.coyle@kroger.com>
2017-05-03 22:50:54 -0500

    Replaced logo image and moved links to right side in non-responsive mode

---
+++ src/data/app-data.js
@@ -0,0 +1,8 @@
+const CONSTANTS = {
+  INDEX_HEADER: 'Nioh Reforging Guide',
+  ARMOR_LINK: 'Armor Reforging Guide',
+  MELEE_LINK: 'Melee Weapon Reforging Guide',
+  RANGED_LINK: 'Ranged Weapon Reforging Guide',
+};
+
+export default CONSTANTS;
